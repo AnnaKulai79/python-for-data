0c5b7c0180fc69e5a96e10daf53964ea76263ab4
AnnaKulai79 <hannahku68@gmail.com>
2025-11-14 14:49:31 +0100

    add figure about count genres

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 url = "./data/movies_metadata.csv"
 df = pd.read_csv(url)
 # print(df.head())
@@ -17,7 +20,25 @@ df["homepage"] = df["homepage"].fillna("No homepage")
 # print(df.homepage)
 df["belongs_to_collection"] = df["belongs_to_collection"].fillna("{}")
 # print(df.belongs_to_collection)
-df.info()
+# df.info()
 df.dropna(inplace=True)
-print(df.isnull().sum())
-df.info()
+# print(df.isnull().sum())
+# df.info()
+
+# print(df.genres)
+genres_count = df["genres"].value_counts()
+# print(genres_count)
+# print(genres_count.index)
+# print(genres_count.values)
+
+plt.figure(figsize=(10,6))
+
+sns.barplot(x=genres_count.index, y=genres_count.values)
+
+plt.title("Count of genres")
+plt.xlabel("Genres")
+plt.ylabel("Counts")
+
+plt.xticks(rotation=45)
+
+plt.show()
